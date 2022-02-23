036e4fdbe5a67b975e55952c4f3b1fc0b38550f4
Lateef Ogunbadejo <lateefogunbadejo@yahoo.com>
2022-02-23 23:24:31 +0100

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { extendTheme, ChakraProvider, Text, Button, Checkbox } from "@chakra-ui/react";
+import appTheme from './theme';;
+
+const theme = extendTheme({
+  ...appTheme,
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Text>Testing font</Text>
+      <Button>Test Button</Button>
+      <Checkbox defaultChecked>Checkbox</Checkbox>
+    </ChakraProvider>
   );
 }
 
